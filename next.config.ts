0a462797d685137b8e0fdb5b8b7f89b033051a4c
Zubair Zafar <umarzafar49678@gmail.com>
2026-08-13 16:05:14 +0500

    TS

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,9 @@
-import type { NextConfig } from "next"; const nextConfig: NextConfig = { output: "export", distDir: "dist", images: { unoptimized: true, }, trailingSlash: true, }; export default nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
